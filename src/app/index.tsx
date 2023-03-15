14a8792f8e01beaa332513aa5fcb51fe2559601d
ISextra <sabinixkensey@gmail.com>
2023-03-15 23:39:08 +0300

    [TASK-2] добавление cass, добавление BrowserRouter, изменение архитектуры на Feature-Sliced Design

---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.sass';
+import { Routes, Route, Link } from "react-router-dom";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                ImageGallery
+            </header>
+        </div>
+    );
+}
+
+export default App;
